e98486afadf4b798667fa0184a2c804c4e29c93a
hp0098v1 <hp0098v1@gmail.com>
2024-01-03 20:17:37 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const form = document.getElementById("form");
+const input = document.getElementById("config");
+const container = document.getElementById("container");
+const message = document.getElementById("message");
+const output = document.getElementById("output");
+const button = document.getElementById("copy-btn");
+
+let newConfigV2ray;
+const NewDomainAndConfig =
+  "node.hp0098v1.online:2096?type=ws&path=%2F&host=cdn.hp0098v1.online&security=tls&fp=chrome&alpn=http%2F1.1%2Ch2&sni=cdn.hp0098v1.online";
+
+input.addEventListener("keyup", (e) => {
+  if (e.target.value === "") {
+    container.style.display = "none";
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = input.value;
+  if (inputValue === "") {
+    alert("لطفا کانفیگتون رو وارد کنید.");
+    return;
+  }
+
+  const uid = inputValue.split("@")[0];
+  const domainAndConfig = inputValue.split("@")[1].split("#")[0];
+  const configName = inputValue.split("@")[1].split("#")[1];
+
+  newConfigV2ray = uid + "@" + NewDomainAndConfig + "#" + configName;
+  message.textContent =
+    "روی دکمه کپی بزنین و سرور جدیدتون را به برنامتون اضافه کنید.";
+  output.textContent = newConfigV2ray;
+  container.style.display = "block";
+});
+
+button.addEventListener("click", function (e) {
+  if (newConfigV2ray === undefined) {
+    alert("لطفا کانفیگتون رو وارد کنید.");
+    return;
+  }
+  e.stopPropagation();
+
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(newConfigV2ray);
+    alert("سرور کپی شد");
+    return; //codes below wont be executed
+  }
+
+  output.focus();
+  output.select();
+
+  document.execCommand("copy");
+});
