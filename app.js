5158e2e166226fccc3adaebbc2ee817afff1e28f
hp0098v1 <hp0098v1@gmail.com>
2024-02-11 19:09:04 +0330

    change config

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const output = document.getElementById("output");
 const button = document.getElementById("copy-btn");
 
 let newConfigV2ray;
-const NewDomainAndConfig =
-  "node.in-network.top:2096?type=ws&path=%2F&host=cdn.in-network.top&security=tls&fp=chrome&sni=cdn.in-network.top";
 
 input.addEventListener("keyup", (e) => {
   if (e.target.value === "") {
@@ -24,13 +22,166 @@ form.addEventListener("submit", (e) => {
     return;
   }
 
-  const uid = inputValue.split("@")[0];
-  const domainAndConfig = inputValue.split("@")[1].split("#")[0];
-  const configName = inputValue.split("@")[1].split("#")[1];
+  const uid = inputValue.split("@")[0].split("vless://")[1];
+  // const domainAndConfig = inputValue.split("@")[1].split("#")[0];
+  // const configName = inputValue.split("@")[1].split("#")[1];
 
-  newConfigV2ray = uid + "@" + NewDomainAndConfig + "#" + configName;
   message.textContent = "متن سرور را کپی و به برنامه ی خود اضافه کنید";
-  output.textContent = newConfigV2ray;
+  output.textContent = `{
+    "dns": {
+      "hosts": {
+        "domain:googleapis.cn": "googleapis.com"
+      },
+      "servers": [
+        "1.1.1.1"
+      ]
+    },
+    "inbounds": [
+      {
+        "listen": "127.0.0.1",
+        "port": 10808,
+        "protocol": "socks",
+        "settings": {
+          "auth": "noauth",
+          "udp": true,
+          "userLevel": 8
+        },
+        "sniffing": {
+          "destOverride": [
+            "http",
+            "tls"
+          ],
+          "enabled": true
+        },
+        "tag": "socks"
+      },
+      {
+        "listen": "127.0.0.1",
+        "port": 10809,
+        "protocol": "http",
+        "settings": {
+          "userLevel": 8
+        },
+        "tag": "http"
+      }
+    ],
+    "log": {
+      "loglevel": "warning"
+    },
+    "outbounds": [
+      {
+        "mux": {
+          "concurrency": 8,
+          "enabled": true,
+          "xudpConcurrency": 0,
+          "xudpProxyUDP443": "reject"
+        },
+        "protocol": "vless",
+        "settings": {
+          "vnext": [
+            {
+              "address": "PaY.ChaRjE.OnLiNe",
+              "port": 2096,
+              "users": [
+                {
+                  "encryption": "none",
+                  "flow": "",
+                  "id": "${uid}",
+                  "level": 8,
+                  "security": "auto"
+                }
+              ]
+            }
+          ]
+        },
+        "streamSettings": {
+          "network": "ws",
+          "security": "tls",
+          "tlsSettings": {
+            "allowInsecure": true,
+            "fingerprint": "chrome",
+            "publicKey": "",
+            "serverName": "cDN.HPVIp.ONLiNE",
+            "shortId": "",
+            "show": false,
+            "spiderX": ""
+          },
+          "wsSettings": {
+            "headers": {
+              "Host": "cDN.HPVIp.ONLiNE"
+            },
+            "path": "/"
+          },
+          "sockopt": {
+            "dialerProxy": "fragment",
+            "tcpKeepAliveIdle": 100,
+            "tcpNoDelay": true
+          }
+        },
+        "tag": "proxy"
+      },
+      {
+        "tag": "fragment",
+        "protocol": "freedom",
+        "settings": {
+          "domainStrategy": "AsIs",
+          "fragment": {
+            "packets": "tlshello",
+            "length": "100-200",
+            "interval": "10-20"
+          }
+        },
+        "streamSettings": {
+          "sockopt": {
+            "tcpKeepAliveIdle": 100,
+            "tcpNoDelay": true
+          }
+        }
+      },
+      {
+        "protocol": "freedom",
+        "settings": {},
+        "tag": "direct"
+      },
+      {
+        "protocol": "blackhole",
+        "settings": {
+          "response": {
+            "type": "http"
+          }
+        },
+        "tag": "block"
+      }
+    ],
+    "policy": {
+      "levels": {
+        "8": {
+          "connIdle": 300,
+          "downlinkOnly": 1,
+          "handshake": 4,
+          "uplinkOnly": 1
+        }
+      },
+      "system": {
+        "statsOutboundUplink": true,
+        "statsOutboundDownlink": true
+      }
+    },
+    "routing": {
+      "domainStrategy": "AsIs",
+      "rules": [
+        {
+          "ip": [
+            "1.1.1.1"
+          ],
+          "outboundTag": "proxy",
+          "port": "53",
+          "type": "field"
+        }
+      ]
+    },
+    "stats": {}
+  }`;
   container.style.display = "block";
 });
 
